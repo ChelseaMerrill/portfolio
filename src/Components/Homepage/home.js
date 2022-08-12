c6504319796988a495512e41b566ee3634038e6b
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-08-12 15:43:55 -0400

    set up grid

--- src/Components/Homepage/home.js
+++ src/Components/Homepage/home.js
@@ -1,9 +1,53 @@
-const Home = () => {
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './home.css';
+
+const About = () => {
     return(
         <div>
-            <h1>Test</h1>
+            <p>Hello, I am Chelsea, a full-stack software engineer with two year of experience designing and developing applications as well as managing teams of developers.  I specialize in front-end frameworks and am ready to help you launch your next new idea </p>
         </div>
+    )
+}
 
+const Home = () => {
+    return (
+        <div>
+            <Container className='home-page'>
+                <Row>
+                    <Col xs={9} md={6} className='box'>
+                        <h1>hello</h1>
+                    </Col>
+                    <Col xs={9} md={6} className='box'>
+                        <h1>About Me</h1>
+                        {About()}
+                    </Col>
+                </Row>
+
+
+                <Row>
+                    <Col xs={9} md={4} className='box'>
+                        <h1>projects</h1>
+                    </Col>
+                    <Col xs={9} md={4} className='box'>
+                        <h1>picture</h1>
+                    </Col>
+                    <Col xs={9} md={4} className='box'>
+                        <h1>resume</h1>
+                    </Col>
+                </Row>
+
+
+                <Row>
+                    <Col xs={9} md={6} className='box'>
+                        <h1>contact</h1>
+                    </Col>
+                    <Col xs={9} md={6} className='box'>
+                        <h1>photo</h1>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 }
 
